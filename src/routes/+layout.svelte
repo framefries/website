6dc700921d4245e542946d924f2a624719e4c8af
noclat <noclat@gmail.com>
2023-08-15 14:38:29 +0200

    fix:static assets relative paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
 	<meta property="og:title" content={data.title} />
 	<meta property="og:description" content={data.description} />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="/framefries-opengraph.png" />
-	<meta property="og:image:alt" content="/framefries-opengraph.png" />
+	<meta property="og:image" content="framefries-opengraph.png" />
+	<meta property="og:image:alt" content="framefries-opengraph.png" />
 	<meta name="robots" content={data.indexing} />
 </svelte:head>
 
