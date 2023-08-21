e85c51dd937bb99c7da2c1f09451b621370f57ce
noclat <noclat@gmail.com>
2023-08-21 18:16:21 +0200

    feat:divider component

---
+++ src/lib/components/Divider.story.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Divider from './Divider.svelte';
+
+  export let Hst;
+  
+  let color = '#292524';
+</script>
+
+<Hst.Story title="Divider">
+  <Hst.Variant title="default">
+    <div style={`color: ${color};`}>
+      <Divider />
+    </div>
+
+    <svelte:fragment slot="controls">
+      <Hst.ColorSelect
+        bind:value={color}
+        title="Color"
+      />
+    </svelte:fragment>
+  </Hst.Variant>
+</Hst.Story>
---
+++ src/lib/components/Divider.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div class={`text-center ${$$props.class}`}>
+  <svg
+    viewBox="0 0 130 30"
+    stroke-linecap="round"
+    class="inline-flex w-auto h-4 md:h-[30px] stroke-current fill-none select-none"
+  >
+    <path d="M72.5 29.5H96m-39.5 0h12m-28 0h12m-28 0h12m-22 0h6m-13 0h3m-10 0h3m100.5-9c4.5-3.83 9.5-3.5 18-12" />
+    <path d="M99.5 29.5c3-8.67 13.2-27 30-29" />
+    <path d="M103.5 21c8.83-1.33 26-5 26-20.5" />
+  </svg>
+</div>
