38b4cd3cb854bc0091f64919a806482ac3d6fbd3
noclat <noclat@gmail.com>
2023-08-30 13:42:03 +0200

    feat:pricing range and image

--- src/lib/layouts/Pricing.svelte
+++ src/lib/layouts/Pricing.svelte
@@ -8,22 +8,15 @@
   import RadioGroup from '$lib/components/RadioGroup.svelte';
   import Tooltip from '$lib/components/Tooltip.svelte';
 
-  let values = {
-    size: 'S',
-    details: 1,
-    animated: false,
-    characters: false,
-  };
-
   const fields = [
     {
       value: 'size',
       title: `How big do you want it?`,
       tooltip: `Think of terms like - a small icon, a collection of items or a full scene or an environment.`,
       options: [
-        { value: 'S', label: 'Small' },
-        { value: 'M', label: 'Medium' },
-        { value: 'L', label: 'Large' },
+        { value: 0, label: 'Small' },
+        { value: 1, label: 'Medium' },
+        { value: 2, label: 'Large' },
       ],
     },
     {
@@ -31,9 +24,9 @@
       title: `How detailed do you want them?`,
       tooltip: `Difference between Highly and Hardly is seeing a trees individual leaves vs seeing a tree.`,
       options: [
-        { value: 1, label: 'Hardly' },
-        { value: 2, label: 'Slightly' },
-        { value: 3, label: 'Highly' },
+        { value: 0, label: 'Hardly' },
+        { value: 1, label: 'Slightly' },
+        { value: 2, label: 'Highly' },
       ],
     },
     {
@@ -41,8 +34,8 @@
       title: `Need ‘em animated too?`,
       tooltip: `Animation can add a lot of upfront costs to your project - if you’re unsure then leave it out for now.`,
       options: [
-        { value: false, label: 'Nope' },
-        { value: true, label: 'Yes, please' },
+        { value: 0, label: 'Nope' },
+        { value: 1, label: 'Yes, please' },
       ],
     },
     {
@@ -50,18 +43,27 @@
       title: `Want to add characters too?`,
       tooltip: `Characters can add a lot of complexity.`,
       options: [
-        { value: false, label: 'Nah, I’m good' },
-        { value: true, label: 'Sure!' },
+        { value: 0, label: 'Nah, I’m good' },
+        { value: 1, label: 'Sure!' },
       ],
     },
   ];
 
-  const examples = {
-    S: ExampleSmall,
-    M: ExampleMedium,
-    L: ExampleLarge,
+  let values = {
+    size: 0,
+    details: 0,
+    animated: 0,
+    characters: 0,
   };
 
+  function computePrice({ size, details, animated, characters }, mod = 0) {
+    return (400+(size+1+mod)*400+details*200)*(1+animated+characters);
+  }
+
+  $: priceLow = computePrice(values);
+  $: priceHigh = computePrice(values, 1);
+
+  const examples = [ExampleSmall, ExampleMedium, ExampleLarge];
   $: exampleSrc = examples[values.size] || ExampleSmall;
 </script>
 
@@ -71,11 +73,10 @@
     'p-6 md:p-12 lg:px-20 lg:py-16',
     'flex gap-28',
     'md:rounded-md bg-accent',
-    'text-cream',
     $$props.class
   )}
 >
-  <div class="space-y-14">
+  <div class="space-y-14 text-cream">
     <header class="space-y-4 md:text-xl">
       <h2 class="text-3xl md:text-4xl lg:text-5xl md:!leading-tight font-display">
         Now, what’s the cost?
@@ -114,19 +115,25 @@
   </div>
 
   <div class="shrink-0 relative w-[416px]">
-    <div class="absolute inset-x-0 -top-[calc(4rem_+_3px)] bottom-0 flex flex-col">
-      <div class="grow p-8 rounded-t bg-cream">
-        <div class="rounded-md">
-          <img
-            src={exampleSrc}
-            alt={`Illustration example size ${values.size}`}
-            class="block w-full h-auto"
-          />
-        </div>
+    <div class="absolute inset-x-0 -top-[calc(4rem_+_3px)] bottom-0 flex flex-col items-stretch">
+      <div class="grow flex flex-col gap-4 px-8 pt-16 rounded-t bg-cream">
+        <img
+          src={exampleSrc}
+          alt="Illustration size comparison"
+          class="grow rounded-md bg-[#F9F3E3] object-contain"
+        />
+        <p class="py-4 flex flex-col gap-1 text-center">
+          <span class="md:text-xl">Your total might come to</span>
+          <span class="md:text-3xl text-accent font-display whitespace-nowrap">
+            {priceLow.toLocaleString('en-US')} – {priceHigh.toLocaleString('en-US')} €
+          </span>
+        </p>
       </div>
-      <svg viewBox="0 0 416 67" class="-mt-1 w-full h-auto stroke-0 fill-cream">
-        <path d="M0 60.75V0h416v60.75a5.3 5.3 0 0 1-6.9 5.05 662.36 662.36 0 0 0-402.2 0A5.3 5.3 0 0 1 0 60.75Z" />
-      </svg>
+      <div>
+        <svg viewBox="0 0 416 67" class="-mt-1 w-full h-auto stroke-0 fill-cream">
+          <path d="M0 60.75V0h416v60.75a5.3 5.3 0 0 1-6.9 5.05 662.36 662.36 0 0 0-402.2 0A5.3 5.3 0 0 1 0 60.75Z" />
+        </svg>
+      </div>
     </div>
   </div>
 </section>
