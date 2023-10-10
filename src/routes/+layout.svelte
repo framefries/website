6694daa3e3b481c0ff7c484a4c69d4195e25f824
noclat <noclat@gmail.com>
2023-10-10 19:46:00 +0200

    feat: add google tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,17 @@
 	<meta property="og:image" content={OGImage} />
 	<meta property="og:image:alt" content={data.title} />
 	<meta name="robots" content={data.indexing} />
+	{#if import.meta.env.VITE_GTAG_ID}
+		<script async src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GTAG_ID}`} /></script>
+		{@html `
+			<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', '${import.meta.env.VITE_GTAG_ID}');
+			</script>
+		`}
+	{/if}
 </svelte:head>
 
 <slot />
