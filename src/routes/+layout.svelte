48b01311192dcdc44f166b56d7a27f0703404ba3
noclat <noclat@gmail.com>
2023-10-10 19:46:00 +0200

    feat: add google tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,17 +14,28 @@
 	<meta property="og:image" content={OGImage} />
 	<meta property="og:image:alt" content={data.title} />
 	<meta name="robots" content={data.indexing} />
-	{#if import.meta.env.VITE_GTAG_ID}
-		<script async src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.VITE_GTAG_ID}`} /></script>
+	{#if import.meta.env.VITE_GTM_ID}
 		{@html `
-			<script>
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);}
-				gtag('js', new Date());
-				gtag('config', '${import.meta.env.VITE_GTAG_ID}');
-			</script>
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${import.meta.env.VITE_GTM_ID}');</script>
 		`}
 	{/if}
 </svelte:head>
+
+{#if import.meta.env.VITE_GTM_ID}
+<!-- svelte-ignore a11y-missing-attribute -->
+<noscript>
+	<iframe
+		src={`https://www.googletagmanager.com/ns.html?id=${import.meta.env.VITE_GTM_ID}`}
+		height="0"
+		width="0"
+		style="display:none;visibility:hidden"
+	>
+	</iframe>
+</noscript>
+{/if}
 
 <slot />
